c3c11e4ed6ee5bde30ce37c1625577f011476a5c
TiagoMSCarvalho <95222018+TiagoMSCarvalho@users.noreply.github.com>
2022-01-26 12:36:31 +0000

    Delete pk-EP directory

--- pk-EP/Auxiliary Functions/SkewMatrix4.m
+++
@@ -1,9 +0,0 @@
-function [skewmatrix4] = SkewMatrix4(vector)
-    %Transforms the vector into a 4x4 SkewMatrix for the Pos and Accel
-    %Analysis ( Minus Skew Matrix)
-    
-    skewmatrix4 = [0, -vector';
-                   vector, -SkewMatrix3(vector)];
-    
-end
-
--- pk-EP/Post Processing Parameters Functions/compute_wb.m
+++
@@ -1,7 +0,0 @@
-function [Track_F,Track_R,WheelBase_L,WheelBase_R]=compute_wb(Points,i)
-
-Track_F=Points{11,2}(i)-Points{23,2}(i);
-Track_R=Points{35,2}(i)-Points{47,2}(i);
-
-WheelBase_L=Points{11,1}(i)-Points{35,1}(i);
-WheelBase_R=Points{23,1}(i)-Points{47,1}(i);
