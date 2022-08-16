51e719ba74a108b1ae76ad938d63815aa80a4afb
TiagoMSCarvalho <tiagomscarvalho@edu.ulisboa.pt>
2022-08-16 18:20:14 +0100

    ALF Exp

---
+++ mbs-EP-v.1.0.2 - ALF exp/Force Elements/Force_Damper.m
@@ -0,0 +1,113 @@
+function [forceel3] = Force_Damper(forcescount,NBodies,Bodies,Damper,ForceFunction,forceel3,coord)
+%% Initial variable definitions
+%forceel
+if coord == 7
+    forceel = zeros(7*NBodies,1);
+elseif coord == 6
+    forceel = zeros(6*NBodies,1);
+end
+% Bodies numbers
+i = Damper(forcescount).Body1;
+j = Damper(forcescount).Body2;
+% Bodies position vectors
+ri = Impose_Column(Bodies(i).r);
+rj = Impose_Column(Bodies(j).r);
+% Force Element location relative to each Bodies coordinate system
+spi = Impose_Column(Damper(forcescount).spi);
+spj = Impose_Column(Damper(forcescount).spj);
+% Rotation matrix for each Bodies
+Ai = Bodies(i).A;
+Aj = Bodies(j).A;
+% Joint location in the global/absolute coordinate system but in relation
+% to the CoM (Pre Processing sp - r)
+spig = Ai*spi;
+spjg = Aj*spj;
+%% Displacement Derivative
+% Translational Velocities
+rdi = Bodies(i).rd;
+rdj = Bodies(j).rd;
+% Angular Velocities
+wi = Bodies(i).w;
+wj = Bodies(j).w;
+% Angular Velocities Skew Matrices
+swi = SkewMatrix3(wi);
+swj = SkewMatrix3(wj);
+% Derivatives of the global spi and spj
+spid = swi*Ai*spi;
+spjd = swj*Aj*spj;
+%Skew spi and spj
+sspi = SkewMatrix3(spi);
+sspj = SkewMatrix3(spj);
+
+%% Vector Calculus and formulation
+% Displacement and Delta Calculus
+displacement = rj + spjg - ri - spig;    
+% Displacement derivative
+ddisp = rdj + spjd - rdi - spid;
+% Force Direction Vector
+[lmag,lun] = unitvector(displacement);
+lengthrateofchange = (1/lmag)*(displacement'*ddisp);
+% Force Magnitude Calculus
+if ~isnan(Damper(forcescount).Constant)
+    % Linear Spring
+    c = Damper(forcescount).Constant; %damping coefficient
+    force = c*lengthrateofchange;
+elseif isnan(Damper(forcescount).Constant)
+    % Non Linear Spring:
+        %Function Input: Velocity of compression ( lengthrateofchange)
+        %Function Output: Damper Force
+    sym x
+    Noffun = Damper(forcescount).Noffun;
+    if Noffun == 1
+        dfunc = str2func(Damper(forcescount).Function1);
+        force = dfunc(lengthrateofchange);
+    elseif Noffun == 2
+        if lengthrateofchange <= Damper(forcescount).Intmin
+            dfunc = str2func(ForceFunction.Damper(forcescount).Function1);
+            force = dfunc(lengthrateofchange);
+        elseif lenthrateofchange > Damper(forcescount).Intmin
+            dfunc = str2func(ForceFunction.Damper(forcescount).Function2);
+            force = dfunc(lenthrateofchange);
+        end
+    elseif Noffun == 3
+        if lengthrateofchange <= Damper(forcescount).Intmin
+            dfunc = str2fun(ForceFunction.Damper(forcescount).Function1);
+            force = dfunc(lengthrateofchange);
+        elseif lengthrateofchange < Damper(forcescount).Intmax && lengthrateofchange > Damper(forcescount).Intmin
+            dfunc = str2fun(ForceFunction.Damper(forcescount).Function2);
+            force = dfunc(lengthrateofchange);
+        elseif lengthrateofchange >= Damper(forcescount).Intmax
+            dfunc = str2fun(ForceFunction.Damper(forcescount).Function3);
+            force = dfunc(lengthrateofchange);
+        end
+    end
+end
+%Force Vectors
+forcei = force*lun;
+forcej = -force*lun;
+%Moments Created by the Damper
+momenti = sspi*Ai'*forcei;
+momentj = sspj*Aj'*forcej;
+% Allocation of the force to the vector
+if coord == 7
+    % Body i
+    i1 = 7*(i-1)+1;
+    forceel(i1:i1+2,1) = forcei;
+    forceel(i1+3:i1+6,1) = 2*Bodies(i).L'*momenti;
+    % Body j
+    i2 = 7*(j-1)+1;
+    forceel(i2:i2+2,1) = forcej;
+    forceel(i2+3:i2+6,1) = 2*Bodies(j).L'*momentj;
+elseif coord == 6
+    % Body i
+    i1 = 6*(i-1)+1;
+    forceel(i1:i1+2,1) = forcei;
+    forceel(i1+3:i1+5,1) = momenti;
+    % Body j
+    i2 = 6*(j-1)+1;
+    forceel(i2:i2+2,1) = forcej;
+    forceel(i2+3:i2+5,1) = momentj;
+end
+%Add to the existing vector
+forceel3 = forceel3 + forceel;
+end
---
+++ mbs-EP-v.1.0.2 - ALF exp/Joints and Constraints/Joint_Universal.m
@@ -0,0 +1,162 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Inputs and Outputs:
+%    - fun : the fsolve tool will define value for the variables so that each
+%    expression is equal to zero. The f variable carries all the values of
+%    the expressions (f will be all zeros when solved)
+%    - Jacobian : the Jacobian Matrix (check documentation for more
+%      information)
+%    - niu : right-hand-side of the velocity equations (check documentation
+%      for more information)
+%    - gamma : right-hand-side of the acceleration equations (check documentation
+%      for more information)
+%    - funCount: the equation/function counter
+%    - jointCount: current Joint number on the structure Universal
+%    - Bodies: all the information of the MultiBodies System
+%    - Universal: all the information of the Universal joints present in
+%    the multiBodies system
+%    - Flags: 4 flags that determine which values to be calculated (each
+%    flag for one of the 4 first inputs). 1 means to calculate
+%
+% Objective of the Function:
+%    This functions objective is calculate the values of the expressions
+%    the formulate each specific joints, calculate its Jacobian and
+%    the right-hand-side of both velocity and acceleration equations. Each
+%    calculation will only be made if the correspondent Flag value is 1.
+%    Note that the multiBodies systems' Jacobian is defined with all the joints  
+%    and constraints, which means, each joint just defines a few lines of
+%    this matrix and the values that are calculated are only in the columns
+%    correspondent to the bodies involved
+
+
+function [fun,Jacobian,niu,gamma,funCount] = Joint_Universal(fun,Jacobian,niu,gamma,funCount,jointCount, Bodies, Universal,Flags)
+%% Initial variable definitions
+% Bodies numbers
+i = Universal(jointCount).Body1;
+j = Universal(jointCount).Body2;
+% Bodies position vectors
+ri = Impose_Column(Bodies(i).r);
+rj = Impose_Column(Bodies(j).r);
+% Bodies Euler Parameters
+pi = Impose_Column(Bodies(i).p);
+pj = Impose_Column(Bodies(j).p);
+% Joint location relative to each Bodies coordinate system
+spi = Impose_Column(Universal(jointCount).spi);
+spj = Impose_Column(Universal(jointCount).spj);
+% Definition of the vectors (each one in one of the bodies coordinate
+% system) that should be perpendicular
+si = Impose_Column(Universal(jointCount).si);
+sj = Impose_Column(Universal(jointCount).sj);
+% Rotation matrix for each Bodies
+Ai = Bodies(i).A;
+Aj = Bodies(j).A;
+%Vector si and sj in the global frame
+sig = Ai*si;
+sjg = Aj*sj;
+%Vector sip and sjp in the global frame
+spig = Ai*spi;
+spjg = Aj*spj;
+%SkewMatrix Vector
+ssi= SkewMatrix4(si);
+ssj= SkewMatrix4(sj);
+%SkewMatrix Vector si and sj for P
+sspi = SkewMatrix4(spi);
+sspj = SkewMatrix4(spj);
+% Euler Parameters Aux Identities
+Gi = Bodies(i).G;
+Gj = Bodies(j).G;
+
+%% Joint Formulation - Kinematic Problem
+% Position constraint equations
+if(Flags.Position == 1)
+    fun(funCount:funCount+2,1) = ri + spig - rj - spjg;
+    fun(funCount+3,1) = sig'*sjg;
+end
+
+% Jacobian Matrix
+%Universal 2ª eq é referente ao si e ao sj e nao ao spi e spj normal.
+if (Flags.Jacobian == 1) && (Flags.Dynamic == 0)
+    %Ci Cj written to spi and spj aux calc tab 7.1 Nikra  (pg201)
+    Cpi = 2*(Gi*sspi + spi*pi');
+    Cpj = 2*(Gj*sspj + spj*pj');
+    %Ci Cj written to si and sj
+    Ci = 2*(Gi*ssi + si*pi');
+    Cj = 2*(Gj*ssj + sj*pj');
+    %Body i
+    i1 = 7*(i-1)+1;
+    i2  = i1+6;
+    Jacobian(funCount:funCount+2,i1:i2)=[eye(3), Cpi];
+    Jacobian(funCount+3,i1:i2)=[0,0,0,sjg'*Ci];
+    %Body j
+    i1 = 7*(j-1)+1;
+    i2 = i1+6;
+    Jacobian(funCount:funCount+2,i1:i2)=[-eye(3),-Cpj];
+    Jacobian(funCount+3,i1:i2)=[0,0,0,sig'*Cj];
+end
+
+% Right-hand-side velocity equations
+if(Flags.Velocity == 1)
+    niu(funCount:funCount+3,1) = 0;
+end
+
+% Right-hand-side acceleration equations
+if(Flags.Acceleration == 1)
+    
+    %Taking the G and L derivatives out
+    Gdi = Bodies(i).Gd;
+    Gdj = Bodies(j).Gd;
+    Ldi = Bodies(i).Ld;
+    Ldj = Bodies(j).Ld;
+    %Extract the angular velocity vectors from the Bodies Struct
+    wli = Bodies(i).wl;
+    wlj = Bodies(j).wl;
+    %Derivatives of si and sj in the global frame
+    sid = Ai*SkewMatrix3(wli)*sig;
+    sjd = Aj*SkewMatrix3(wlj)*sjg;
+    
+    gamma(funCount:funCount+2,1) = 2*Gdj*Ldj.'*spj - 2*Gdi*Ldi.'*spi;
+    gamma(funCount+3,1) = sig'*(-2*Gdj*Ldj.'*sj) + sjg'*(-2*Gdi*Ldi.'*si) - 2*sid'*sjd;
+end
+%% Joint Formulation - Dynamic Problem
+%Jacobian Matrix
+if (Flags.Jacobian == 1) && (Flags.Dynamic == 1)
+    %1 SPH, 1x n1,1 constraint equations
+    %Skew Matrix 3x3
+    skewspi = SkewMatrix3(spig);
+    skewspj = SkewMatrix3(spjg);
+    skewsi = SkewMatrix3(sig);
+    skewsj = SkewMatrix3(sjg);
+    %Body i
+    i1 = 6*(i-1)+1;
+    i2  = i1+5;
+    Jacobian(funCount:funCount+2,i1:i2)=[eye(3),-skewspi*Ai];
+    Jacobian(funCount+3,i1:i2)=[0,0,0,-sjg'*skewsi*Ai];
+    %Body j
+    i1 = 6*(j-1)+1;
+    i2 = i1+5;
+    Jacobian(funCount:funCount+2,i1:i2)=[-eye(3),skewspj*Aj];
+    Jacobian(funCount+3,i1:i2)=[0,0,0,-sig'*skewsj*Aj];
+end
+
+if(Flags.AccelDyn == 1)
+    %Body i, dynamic pre processing
+    wi = Bodies(i).w;
+    %Body j, dynamic pre processing
+    wj = Bodies(j).w;
+    %Angular Vel Skew Matrices
+    swi = SkewMatrix3(wi);
+    swj = SkewMatrix3(wj);
+    %Derivatives of the sp's
+    spid = swi*spig;
+    spjd = swj*spjg;
+    %Derivatives of qi,ti and sj in the global frame
+    sid = swi*sig;
+    sjd = swj*sjg;
+    
+    gamma(funCount:funCount+2,1) = -swi*spid + swj*spjd;
+    gamma(funCount+3,1) = -2*sid'*sjd + sid'*swi*sjg + sjd'*swj*sig;
+end
+
+%% Update the function counter
+funCount = funCount+4;
+end
